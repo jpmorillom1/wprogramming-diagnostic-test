448755a6d7c3881f74582ae663bb946655932654
jpmorillom1 <morillojuanpablo2003@gmail.com>
2025-11-27 16:48:20 -0500

    feat(security): allow public access to stress endpoints

--- back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/controller/StressController.java
+++ back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/controller/StressController.java
@@ -1,15 +1,17 @@
 package com.uce.jp.diagnostic_test_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/stress")
 public class StressController {
 
     @GetMapping("/cpu")
-    public String stressCpu(){
+    public String stressCpu() {
+        System.out.println("Initializing stress process");
         double result = 0;
         for (int i = 0; i < 1000000; i++) {
             result += Math.sqrt(i);
--- back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/security/SecurityConfig.java
+++ back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/security/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/user/**").permitAll()
                         .requestMatchers("/api/user/history").authenticated()
                         .requestMatchers("/api/pokemon/**").authenticated()
+                        .requestMatchers("/stress/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
