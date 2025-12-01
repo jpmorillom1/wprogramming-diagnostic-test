375a20e85aa81d0595591816ef9dd076fab3c7eb
jpmorillom1 <morillojuanpablo2003@gmail.com>
2025-12-01 12:36:53 -0500

    feat(controller): add student IDs to the student data initialization

--- back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/controller/StudentsController.java
+++ back/diagnostic-test-backend/src/main/java/com/uce/jp/diagnostic_test_backend/controller/StudentsController.java
@@ -17,20 +17,20 @@ public class StudentsController {
     private static final Map<Long, Map<String, Object>> students = new HashMap<>();
 
     static {
-        students.put(1L, Map.of("firstName", "Luis Gonzalo", "lastName", "Achig Carcelen", "age", 24));
-        students.put(2L, Map.of("firstName", "Jessiel Josue", "lastName", "Chasiguano Santin", "age", 22));
-        students.put(3L, Map.of("firstName", "Bryan Fabricio", "lastName", "Chileno Agualongo", "age", 23));
-        students.put(4L, Map.of("firstName", "Kleber Alejandro", "lastName", "Chiliquinga Lara", "age", 25));
-        students.put(5L, Map.of("firstName", "Kenny Israel", "lastName", "Cisneros Calderon", "age", 22));
-        students.put(6L, Map.of("firstName", "Davinson Mayer", "lastName", "Diaz Tapia", "age", 24));
-        students.put(7L, Map.of("firstName", "Juan Pablo", "lastName", "Morillo Macias", "age", 23));
-        students.put(8L, Map.of("firstName", "Gissela Dayana", "lastName", "Muzo Anchapaxi", "age", 21));
-        students.put(9L, Map.of("firstName", "Abel Eduardo", "lastName", "Navarrete Giler", "age", 25));
-        students.put(10L, Map.of("firstName", "Liz Daisy", "lastName", "Pillajo Cabrera", "age", 20));
-        students.put(11L, Map.of("firstName", "Joseph Alexander", "lastName", "Ponce Naranjo", "age", 24));
-        students.put(12L, Map.of("firstName", "Rodrigo Javier", "lastName", "Saransig Briones", "age", 23));
-        students.put(13L, Map.of("firstName", "Alex Armando", "lastName", "Tituana Ushina", "age", 22));
-        students.put(14L, Map.of("firstName", "Jonathan Rolando", "lastName", "Villarreal Cardenas", "age", 25));
+        students.put(1L, Map.of("id", 1L, "firstName", "Luis Gonzalo", "lastName", "Achig Carcelen", "age", 24));
+        students.put(2L, Map.of("id", 2L, "firstName", "Jessiel Josue", "lastName", "Chasiguano Santin", "age", 22));
+        students.put(3L, Map.of("id", 3L, "firstName", "Bryan Fabricio", "lastName", "Chileno Agualongo", "age", 23));
+        students.put(4L, Map.of("id", 4L, "firstName", "Kleber Alejandro", "lastName", "Chiliquinga Lara", "age", 25));
+        students.put(5L, Map.of("id", 5L, "firstName", "Kenny Israel", "lastName", "Cisneros Calderon", "age", 22));
+        students.put(6L, Map.of("id", 6L, "firstName", "Davinson Mayer", "lastName", "Diaz Tapia", "age", 24));
+        students.put(7L, Map.of("id", 7L, "firstName", "Juan Pablo", "lastName", "Morillo Macias", "age", 23));
+        students.put(8L, Map.of("id", 8L, "firstName", "Gissela Dayana", "lastName", "Muzo Anchapaxi", "age", 21));
+        students.put(9L, Map.of("id", 9L, "firstName", "Abel Eduardo", "lastName", "Navarrete Giler", "age", 25));
+        students.put(10L, Map.of("id", 10L, "firstName", "Liz Daisy", "lastName", "Pillajo Cabrera", "age", 20));
+        students.put(11L, Map.of("id", 11L, "firstName", "Joseph Alexander", "lastName", "Ponce Naranjo", "age", 24));
+        students.put(12L, Map.of("id", 12L, "firstName", "Rodrigo Javier", "lastName", "Saransig Briones", "age", 23));
+        students.put(13L, Map.of("id", 13L, "firstName", "Alex Armando", "lastName", "Tituana Ushina", "age", 22));
+        students.put(14L, Map.of("id", 14L, "firstName", "Jonathan Rolando", "lastName", "Villarreal Cardenas", "age", 25));
     }
 
 
